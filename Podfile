040ee2ca628c8f3a2c95c9893e6deceaf570006f
Jesus Victorio <jesus.victorio@geographica.gs>
2018-11-15 16:07:42 +0100

    project example with sdkTempo

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+
+target 'Tempo-ios-example' do
+  	use_frameworks!
+	pod 'Tempo-ios-sdk', :git => 'git@github.com:jesusgeographica/testSDKTempo.git'
+
+end
---
+++ Tempo-ios-example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Tempo-ios-example
+//
+//  Created by Javier Aragón Encina on 18/10/18.
+//  Copyright © 2018 Javier Aragón Encina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: MapViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initMap()
+        loadMap()
+    }
+}
